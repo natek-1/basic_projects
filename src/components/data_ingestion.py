583fadc19117275b0f0892a9ff7fe36f380431ef
natek-1 <nategabrielk@icloud.com>
2023-07-06 19:25:38 -0400

    Started working on data_ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,1 +1,12 @@
-## All script related to getting the data from a a given source
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+
+@dataclass
+class DataIngestionConfig():
+    train_data_path: str=os.path.join('artifacts', 'train.csv')
