e957a4045ebaea22ca856216c1b3b1655ecb69c1
natek-1 <nategabrielk@icloud.com>
2023-07-08 12:28:14 -0400

    done implmenting initiate_data_transformation from DataTransformation_object

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -62,3 +62,46 @@ class DataTransformation:
             return preprocessor
         except Exception as e:
             raise CustomException(e, sys)
+        
+    def initiate_data_transformation(self, train_path, test_path):
+        try:
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+
+            logging.info("The train and test data were read")
+
+            logging.info("Obtaining preprocessing object")
+
+            preprocessing_obj = self.get_data_transformer_object()
+
+            target_column_name = "math_score"
+            numerical_columns = ["writing_score", "reading_score"]
+
+            input_feature_train_df = train_df.drop(columns=[target_column_name], axis=1)
+            target_feature_train_df = train_df[target_column_name]
+
+            input_feature_test_df = test_df.drop(columns=[target_column_name], axis=1)
+            target_feature_test_df = test_df[target_column_name]
+
+            logging.info(f"Applying Preprocessing object on training and testing dataframe")
+
+            input_feature_train_array = preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_array = preprocessing_obj.transform(input_feature_test_df)
+
+            train_array = np.c_[
+                input_feature_train_array, np.array(target_feature_train_df)
+            ]
+
+            test_array = np.c_[
+                input_feature_test_array, np.array(target_feature_test_df)
+            ]
+            logging.info(f"Saved preprocessing object")
+
+            save_object(
+                file_path=self.data_trasformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj
+            )
+            return (train_array, test_array, self.data_trasformation_config.preprocessor_obj_file_path)
+        
+        except Exception as e:
+            raise CustomException(e, sys)
