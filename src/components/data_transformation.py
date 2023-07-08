5b9588cb72713192602f6ad38824cb2d4962f24f
natek-1 <nategabrielk@icloud.com>
2023-07-08 11:38:28 -0400

    completed the get_data_transformer_object method for DataTransformation class

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,1 +1,64 @@
-# handle diffrent parts of the data (ie one hot encoding, missing values, train, validation and test etc) 
+import sys
+from dataclasses import dataclass
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from src.exception import CustomException
+from src.logger import logging
+import os
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path = os.path.join("artifacts", 'preprocessor.pkl')
+
+class DataTransformation:
+    def __init__(self):
+        self.data_trasformation_config = DataTransformationConfig()
+    
+    def get_data_transformer_object(self):
+        '''
+        This function is responsible for data tranformation
+        '''
+        try:
+            numerical_columns = ["writing_score", "reading_score"]
+            categorical_columns = [
+                "gender",
+                "race_ethnicity",
+                "parental_level_of_education",
+                "lunch",
+                "test_preparation_course",
+            ]
+
+            logging.info(f'Categorical columns: {categorical_columns}')
+            logging.info(f'Numerical columns: {numerical_columns}')
+
+            num_pipeline = Pipeline(
+                steps=[
+                    ("imputer", SimpleImputer(strategy="median")), # Handling missing values
+                    ('scaler', StandardScaler()) # Scaling the data
+                ]
+            )
+
+            logging.info('Numerical column scaling colpleted')
+
+            cat_pipeline = Pipeline(
+                steps=[
+                    ('imputer', SimpleImputer(strategy="most_frequent")),
+                    ("one_hot_encoder", OneHotEncoder()),
+                    ('scaler', StandardScaler()) 
+                ]
+            )
+            logging.info("Categorical coumns encoding completed")
+
+            preprocessor = ColumnTransformer(
+                [
+                    ('num_pipline', num_pipeline, numerical_columns),
+                    ('cat_pipline', cat_pipeline, categorical_columns)
+                ]
+            )
+            return preprocessor
+        except Exception as e:
+            raise CustomException(e, sys)
