2672b3409b1bee0d4e85e898b2c1a22770107129
natek-1 <nategabrielk@icloud.com>
2023-07-11 07:15:18 -0400

    done with the model trainer file

---
+++ src/components/model_trainer.py
@@ -0,0 +1,121 @@
+import os
+import sys
+from dataclasses import dataclass
+
+from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor, GradientBoostingRegressor
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+#from catboost import CatBoostRegressor
+import numpy as np
+
+from src.exception import CustomException
+from src.logger import logging
+
+from src.utils import save_object, evaluate_model
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path=os.path.join("artifacts", 'model.pkl')
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config =ModelTrainerConfig()
+    
+    def initial_model_trainer(self, train_array, test_array, preprocessor_path):
+        try:
+            logging.info("Split training and test input data")
+
+            X_train, y_train, X_test, y_test = (
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1],
+            )
+
+            models = {
+                    "Linear Regression": LinearRegression(),
+                    "Lasso": Lasso(),
+                    "Ridge": Ridge(),
+                    "K-Neighbors Regressor": KNeighborsRegressor(),
+                    "Decision Tree": DecisionTreeRegressor(),
+                    "Random Forest Regressor": RandomForestRegressor(),
+                    "XGBRegressor": XGBRegressor(),
+                    #"CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                    'AdaBoost Regressor': AdaBoostRegressor()
+                }
+                
+            
+            params={
+                "Linear Regression":{},
+                "Lasso": {
+                    'alpha': np.arange(0.00, 1.0, 0.1)
+                },
+                "Ridge":{
+                    [1e-15,1e-10,1e-8,1e-3,1e-2,1,5,10,20,30,35,40,45,50,55,100]
+                },
+                "K-Neighbors Regressor": {
+                    'n_neighbors':[5, 4, 8, 3, 6, 10],
+                    'algorithm': ['auto', 'ball_tree', 'kd_tree']
+                },
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    'splitter':['best','random'],
+                    'max_features':['sqrt','log2'],
+                    'max_depth': range(10, 150, 10) + None,
+                    'max_leaf_nodes': range(50, 150) + None,
+                },
+                "Random Forest Regressor":{
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256],
+                    'max_depth': range(10, 150, 10) + None,
+                    'max_leaf_nodes': range(50, 150) + None,
+                },                
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                '''"CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },'''
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+            model_report, best_models = evaluate_model(X_train=X_train, y_train=y_train,X_test=X_test, y_test=y_test , models=models, params=params)
+
+            best_model_score = max(model_report.values())
+
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            best_model = best_models[best_model_name]
+
+            if best_model_score < 0.6:
+                raise CustomException("No best model found")
+            logging.info('best model found for the dataset')
+
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+            return best_model_score
+            
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+if __name__ == "__main__":
+    model_trainer = ModelTrainer()
+    score = model_trainer.initial_model_trainer()
+    logging.info(f"best model score found: {score}")
+
+
+
--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,9 @@ import numpy as np
 import pandas as pd
 import dill
 
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
 from src.exception import CustomException
 from src.logger import logging
 
@@ -18,3 +21,25 @@ def save_object(file_path, obj):
             dill.dump(obj, file_obj)
     except Exception as e:
         raise ChildProcessError(e, sys)
+
+def evaluate_model(X_train, y_train, X_test, y_test, models, params):
+    try:
+        report = {}
+        best_models = {}
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            param = params[list(models.key)[i]]
+
+            grid_search = GridSearchCV(model, param, cv=3, scoring='r2', n_jobs=-1, verbose=2)
+
+            grid_search.fit(X_train, y_train)
+
+            y_train_pred = grid_search.best_score_
+            y_test_pred = grid_search.predict(X_test)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(model.keys())[i]] = test_model_score, 
+            best_models[list(model.keys())[i]]  = grid_search.best_estimator_
+        return report, best_models
+    except Exception as e:
+        raise CustomException(e, sys)
